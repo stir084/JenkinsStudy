490cc0fbd1f86e9199139153e2588a5a48e43e81
junho <wnsgh1357@gmail.com>
2022-09-26 01:36:48 +0900

    initial commit

--- src/main/java/com/loose/JenkinsStudy/HelloController.java
+++ src/main/java/com/loose/JenkinsStudy/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("/")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!!");
         return "hello";
